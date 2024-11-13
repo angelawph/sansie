f08db811b3a6bbfd7b244c66ef6e4c69994bcbab
angelawph <35798174+angelawph@users.noreply.github.com>
2024-11-13 23:49:31 +0800

    Added GA Tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,9 +2,28 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/theme";
+import Script from "next/script";
+
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<>
+				<Script
+					strategy="lazyOnload"
+					src="https://www.googletagmanager.com/gtag/js?id=G-KYNHZX376Y"
+				/>
+				<Script id="google-analytics" strategy="lazyOnload">
+					{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-KYNHZX376Y', {
+          page_path: window.location.pathname,
+        });
+      `}
+				</Script>
+			</>
+
 			<body>
 				<AppRouterCacheProvider options={{ enableCssLayer: true }}>
 					<ThemeProvider theme={theme}>
