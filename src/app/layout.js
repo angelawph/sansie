0e15e8ac6279be157762ae8b10cf106daba26847
angelawph <35798174+angelawph@users.noreply.github.com>
2024-11-13 23:06:24 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,20 @@ import theme from "@/theme";
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body>
+		<head>
+	
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KYNHZX376Y"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KYNHZX376Y');
+</script>
+
+		</head>
+		<body>
 				<AppRouterCacheProvider options={{ enableCssLayer: true }}>
 					<ThemeProvider theme={theme}>
 						<CssBaseline />
